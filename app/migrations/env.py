65b2ad97b753630a6203c6ad597a06beac4c8af8
zarossa <mr.kirarik@yandex.ru>
2023-11-01 10:59:26 +0700

    Move rooms to hotels

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -8,8 +8,8 @@ from alembic import context
 from app.config import settings
 from app.database import Base
 from app.hotels.models import Hotel
+from app.hotels.rooms.models import Room
 from app.bookings.models import Booking
-from app.rooms.models import Room
 from app.customers.models import Customer
 
 # this is the Alembic Config object, which provides
