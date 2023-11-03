5748e30ab0bdf0ca95dc6fc76b1f3c9efef1ec75
zarossa <mr.kirarik@yandex.ru>
2023-11-03 13:15:08 +0700

    Add SUser schema

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from app.bookings.schemas import SBooking
 from app.bookings.services import BookingService
 from app.users.dependencies import get_current_user
+from app.users.schemas import SUser
 
 router = APIRouter(
     prefix="/bookings",
@@ -11,5 +12,5 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings(user=Depends(get_current_user)) -> list[SBooking]:
+async def get_bookings(user: SUser = Depends(get_current_user)) -> list[SBooking]:
     return await BookingService.find_all(user_id=user.id)
--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, EmailStr
 
 
+class SUser(BaseModel):
+    id: int
+    email: EmailStr
+    hashed_password: str
+
+
 class SUserAuth(BaseModel):
     email: EmailStr
     password: str
